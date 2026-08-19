aebc84df21ce1794a5c0fd0e3b12b051f0ac1a0f
Abdelrahman Osama <119906132+Abdelrahmnan-Osama@users.noreply.github.com>
2026-08-19 23:23:10 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0, j = n - 1;
+
+        while(i < j) {
+            if(!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])) {
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])) {
+                cout << s[i] << " " << s[j] << endl;
+                return false;
+            }  
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
