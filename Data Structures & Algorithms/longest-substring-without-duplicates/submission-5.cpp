411aa2573503708099bac9f4ec9fa34b5041c197
Abdelrahman Osama <119906132+Abdelrahmnan-Osama@users.noreply.github.com>
2026-08-24 15:38:20 +0300

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> pos(256, -1);
+        int n = s.size();
+        int longest = 0;
+        for(int l = 0, r = 0; r < n; r++) {
+            if(pos[s[r]] != -1 && pos[s[r]] >= l) {
+                l = pos[s[r]] + 1;
+            }
+            longest = max(longest, r - l + 1);
+            pos[s[r]] = r;
+        }
+        
+        return longest;
+    }
+};
